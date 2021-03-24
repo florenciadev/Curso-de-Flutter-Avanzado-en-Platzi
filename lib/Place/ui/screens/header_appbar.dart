98440941e78384cc663185b0eea386e29dfe5db6
Florencia Agüero <florencia@alternativasinteligentes.com>
2021-03-24 17:47:48 -0300

    bloc + clean

--- lib/Place/ui/screens/header_appbar.dart
+++ lib/Place/ui/screens/header_appbar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'gradient_back.dart';
-import 'card_image_list.dart';
+import 'package:platzi_trips_app/Place/ui/widgets/card_image_list.dart';
+import 'package:platzi_trips_app/widgets/gradient_back.dart';
 
 class HeaderAppBar extends StatelessWidget {
   @override
@@ -9,9 +9,8 @@ class HeaderAppBar extends StatelessWidget {
     return Stack(
       children: <Widget>[
         GradientBack("Bienvenido"),
-        CardImageList()
+        CardImageList(),
       ],
     );
   }
-
 }
